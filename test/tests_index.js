c1e3a96032b665c5ec1bc787679a2ad1641dfb0a
Farah Zaqout <farah.zaqout@gmail.com>
2018-06-12 15:09:25 +0300

    resetting database on each test, istanbul set up properly

--- test/tests_index.js
+++ test/tests_index.js
@@ -2,10 +2,15 @@ const { dbConnection } = require('../src/database/db_connect');
 const tape = require('tape');
 
 /* eslint-disable no-console */
+/* eslint-disable global-require */
 
-dbConnection.on('error', console.error.bind(console, 'error in connecting to the database'))
+dbConnection.on('error', console.error.bind(console, 'error in connecting to the database'));
 
 dbConnection.once('open', () => {
+
+  // clearing the database
+  require('./db_reset');
+
   require('./test_db');
 
   tape.onFinish(() => {
