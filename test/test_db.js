f8ff847468604a8df98c33d447a7dbedcf748c36
Farah Zaqout <farah.zaqout@gmail.com>
2018-06-12 17:11:34 +0300

    uncommented the database test and removed the 1=1 test

--- test/test_db.js
+++ test/test_db.js
@@ -2,23 +2,18 @@ const tape = require('tape');
 const { Users } = require('../src/database/db_build');
 
 
-// tape('saving users into the database', (t) => {
-//   const hani = new Users({
-//     role: 1,
-//     username: 'hani',
-//     password: 'username',
-//     email: 'password',
-//   });
-//   const expected = 'hani';
-//   hani.save().then(() => {
-//     Users.find({ username: 'hani' }).then((res) => {
-//       t.equal(res[0].username, expected, 'the database should contain a user with the username hani');
-//       t.end();
-//     });
-//   }).catch(err => t.fail(err));
-// });
-
-tape('test', (t) => {
-  t.equal(1,1);
-  t.end();
+tape('saving users into the database', (t) => {
+  const hani = new Users({
+    role: 1,
+    username: 'hani',
+    password: 'username',
+    email: 'password',
+  });
+  const expected = 'hani';
+  hani.save().then(() => {
+    Users.find({ username: 'hani' }).then((res) => {
+      t.equal(res[0].username, expected, 'the database should contain a user with the username hani');
+      t.end();
+    });
+  }).catch(err => t.fail(err));
 });
