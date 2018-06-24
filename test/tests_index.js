948b9915aacb41c49f32104c08a6ffc316fa6fee
Farah Zaqout <farah.zaqout@gmail.com>
2018-06-25 02:40:06 +0300

    cleaning code in the test_index file

--- test/tests_index.js
+++ test/tests_index.js
@@ -8,31 +8,30 @@ dbConnection.on('error', console.error.bind(console, 'error in connecting to the
 
 dbConnection.once('open', () => {
   Promise.resolve()
-  .then(() => {
-    // clearing the database.
-    require('./db_reset');
-  })
-  .then(() => {
-    // testing the database schema.
-    require('./test_db');
-  })
-  .then(() => {
-    // adding events
-    require('./controllers/add_events');
-
-  })
-  .then(() => {
-    // testing the error controllers
-    require('./controllers/error');
-  })
-  .then(() => {
-    // testing the home route
-    require('./controllers/home');
-  })
-  .then(() => {
-    // testing the update event controller
-    require('./controllers/update_events');
-  });
+    .then(() => {
+      // clearing the database.
+      require('./db_reset');
+    })
+    .then(() => {
+      // testing the database schema.
+      require('./test_db');
+    })
+    .then(() => {
+      // adding events
+      require('./controllers/add_events');
+    })
+    .then(() => {
+      // testing the error controllers
+      require('./controllers/error');
+    })
+    .then(() => {
+      // testing the home route
+      require('./controllers/home');
+    })
+    .then(() => {
+      // testing the update event controller
+      require('./controllers/update_events');
+    });
 
   tape.onFinish(() => {
     dbConnection.close();
