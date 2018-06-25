f392608651841f52bf3e0d5f5bfd57a3f445e873
Farah Zaqout <farah.zaqout@gmail.com>
2018-06-25 19:11:08 +0300

    fixed the get route test

--- test/controllers/update_events.js
+++ test/controllers/update_events.js
@@ -2,16 +2,21 @@ const tape = require('tape');
 const supertest = require('supertest');
 const app = require('../../src/app');
 const Events = require('../../src/database/event_schema');
-const query = require('../../src/database/queries/update_events_query');
+
 
 // testing the update event route
 tape('testing the update event route', (t) => {
-  supertest(app)
-    .get('/update-event/id')
-    .expect(200)
-    .end((err, res) => {
-      if (err) return t.error(err);
-      t.equal(JSON.stringify(res).includes('</form>'), true, 'render response should include a form element');
-      t.end();
+  Events.find({ title: 'coding for everyone' }).then((res) => {
+    supertest(app)
+      .get(`/update-event/${res[0].id}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return t.error(err);
+        t.equal(JSON.stringify(res).includes('</form>'), true, 'render response should include a form element');
+        t.end();
+      });
+  })
+    .catch((err) => {
+      t.error(err);
     });
 });
