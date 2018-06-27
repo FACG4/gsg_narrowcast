4aec0ebffb6356220055bb222c23c943cccae45e
ishak <ishak-3@hotmail.com>
2018-06-27 13:45:25 +0300

    final changes on view events page

--- src/database/queries/view_events_querie.js
+++ src/database/queries/view_events_querie.js
@@ -1,9 +1,15 @@
 const Events = require('../event_schema');
 
-const viewQuery = (input, callback) => {
-  Events.find(input).then((res) => {
-    callback(null, res);
-  }).catch(err => callback(err));
+const viewQuery = (req, callback) => {
+  if (!callback && typeof req === 'function') {
+    Events.find({}).then((res) => {
+      req(null, res);
+    }).catch(err => req(err));
+  } else if (callback && typeof callback === 'function') {
+    Events.find(req).then((res) => {
+      callback(null, res);
+    }).catch(err => req(err));
+  }
 };
 
 module.exports = viewQuery;
