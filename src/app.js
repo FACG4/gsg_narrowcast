0a60aed80d67589a19880a099f40293b92bae892
ishak <ishak-3@hotmail.com>
2018-06-27 15:41:15 +0300

    nothing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,37 @@
-const { express, path, exphbs, controllers, serverError } = require('./reqs');
+// const { express, path, exphbs, controllers, serverError } = require('./reqs');
+// const bodyParser = require('body-parser');
+//
+// const app = express();
+//
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+// app.engine('hbs', exphbs({
+//   extname: 'hbs',
+//   layoutsDir: path.join(__dirname, 'views', 'layouts'),
+//   partialsDir: path.join(__dirname, 'views', 'partials'),
+//   defaultLayout: 'main',
+// }));
+//
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.set('port', process.env.PORT || 3000);
+// app.use(controllers);
+// app.use(serverError);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+//
+//
+// module.exports = app;
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const controllers = require('./controllers');
+const error = require('./controllers/error');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', exphbs({
@@ -15,9 +44,7 @@ app.engine('hbs', exphbs({
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.set('port', process.env.PORT || 3000);
 app.use(controllers);
-app.use(serverError);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(error.notFound);
+app.use(error.serverError);
 
 module.exports = app;
