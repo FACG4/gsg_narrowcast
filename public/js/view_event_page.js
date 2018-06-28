c6fafb7b3f07a34745977c3c40a80daad3d6f4a2
ishak <ishak-3@hotmail.com>
2018-06-28 10:15:59 +0300

    to farah

--- public/js/view_event_page.js
+++ public/js/view_event_page.js
@@ -1,5 +1,20 @@
 /* global document fetch swal window */
 const del = document.querySelectorAll('.deleteBtn');
+const monthArray = document.querySelectorAll('.month');
+const dayArray = document.querySelectorAll('.day');
+const startTimeArray = document.querySelectorAll('.start-time');
+const hiddenDate = document.querySelectorAll('.hidden-date');
+const hiddenTitle = document.querySelectorAll('.hidden-thing');
+const hiddenDescription = document.querySelectorAll('.hidden-thing');
+const mainTitle = document.querySelectorAll('.main-title');
+const mainDescription = document.querySelectorAll('.main-description');
+const hiddenSpeaker = document.querySelectorAll('.hidden-speaker');
+const speaker = document.querySelectorAll('.speaker');
+const timeStamp = document.querySelectorAll('.timestamp');
+const hiddenEnd = document.querySelectorAll('.hidden-end');
+const hoursSpan = document.querySelectorAll('.hours-span');
+const minutesSpan = document.querySelectorAll('.minutes-span');
+const secondsSpan = document.querySelectorAll('.seconds-span');
 
 del.forEach((item) => {
   item.addEventListener('click', () => {
@@ -28,3 +43,49 @@ del.forEach((item) => {
 
   });
 });
+
+
+const myCountdown = hiddenDate[0].textContent;
+const newDate = new Date(myCountdown).toISOString();
+let remainingTime = (new Date(newDate).getTime()/1000) - (Date.now()/1000);
+
+
+setInterval(() => {
+  if (remainingTime > 0) {
+    remainingTime -=1;
+    console.log(remainingTime);
+  }
+  const remainingSeconds = Math.floor(remainingTime%3600%60)
+  const hours = Math.floor(remainingTime/3600);
+  const minutes = Math.floor(remainingTime%3600/60);
+  hoursSpan.textContent = hours;
+  minutesSpan.textContent = minutes;
+  secondsSpan.textContent = remainingSeconds;
+}, 1000);
+
+
+const startTime = hiddenDate[0].textContent.split(' ')[4].split(':').slice(0, 2).join(':');
+const endTime = hiddenEnd[0].textContent.split(' ')[4].split(':').slice(0, 2).join(':');
+timeStamp.textContent = `${startTime} - ${endTime}`;
+// mainTitle.textContent = 'Upcoming Event: ' + hiddenTitle[0].textContent;
+// mainDescription.textContent = hiddenDescription[0].textContent;
+speaker.textContent = hiddenSpeaker.textContent;
+
+for (let i = 0; i < monthArray.length; i++) {
+  monthArray[i].textContent = hiddenDate[i].textContent.split(' ')[1];
+};
+
+for (let i = 0; i < dayArray.length; i++) {
+  dayArray[i].textContent = hiddenDate[i].textContent.split(' ')[2];
+};
+
+
+for (let i = 0; i < startTimeArray.length; i++) {
+  if (hiddenDate[i].textContent.split(' ')[4].split(':')[0] > 12) {
+    const time = hiddenDate[i].textContent.split(' ')[4].split(':')[0] - 12;
+    startTimeArray[i].textContent = time + ':' + (hiddenDate[i].textContent.split(' ')[4].split(':')[1]) + ' PM';
+  }
+  else if (hiddenDate[i].textContent.split(' ')[4].split(':')[0] < 12) {
+    startTimeArray[i].textContent = hiddenDate[i].textContent.split(' ')[4].split(':').slice(0, 2).join(':') + ' AM';
+  }
+};
